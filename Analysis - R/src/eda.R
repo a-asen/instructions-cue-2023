34f7711b93d4903d937322096d958aed797ee582
Steffen Aasen <98240487+a-asen@users.noreply.github.com>
2023-11-01 23:59:51 +0100

    aw

--- Analysis - R/src/eda.R
+++ Analysis - R/src/eda.R
@@ -1,8 +1,7 @@
 library('ProjectTemplate')
 load.project()
 
-for (dataset in project.info$data)
-{
+for (dataset in project.info$data){
   message(paste('Showing top 5 rows of', dataset))
   print(head(get(dataset)))
 }
