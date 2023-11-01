1d6563d6ca55d09291df5b9d7cceb320cdd30d7d
Steffen Aasen <98240487+a-asen@users.noreply.github.com>
2023-11-01 23:58:01 +0100

    lib script

--- Analysis - R/src/sampling.R
+++ Analysis - R/src/sampling.R
@@ -1,16 +1,24 @@
-
 # Sampling script
-
-diagnostic_length <- \(min, max, reps=1000, experiment_length=24, probability = NULL){
+max_diagnostic_length <- \(min, max, reps=1000, experiment_length=24, probability = NULL){
   n <- as.numeric()
-  for(x in 1:reps){
-    c(n, sum(sample(4:16,experiment_length, T, prob = probability))) -> n
+
+   for(x in 1:reps){
+    c(n, sum(sample(min:max, experiment_length, T, prob = probability))) -> n
   }
-  hist(n)
+  #hist(n)
+
+  if(!is.null(probability)){
+    cat("With the probability distribution: ", probability, fill = T)
+  }
+
+  ggplot(tibble(n), aes(n))+
+    geom_histogram(aes(y=after_stat(density)))+
+    geom_density(fill="black", alpha=.2)+
+    geom_vline(xintercept = mean(n), colour="red")
 }
 
-gen_probability <- \(){
+max_diagnostic_length(4,16, reps = 30, probability = gen_prob(4,16,.1,"log",1))
 
-}
+# Generate probability distribution of the diagnostic run (if relevant)
 
-diagnostic_length(4,16, probability = [])
+
