3db3a73132c40494bc456cf11cadc8df5865ff3f
steff <98240487+a-asen@users.noreply.github.com>
2023-11-09 16:05:19 +0100

    analysis testing

--- Analysis - R/src/pilot testing.R
+++ Analysis - R/src/pilot testing.R
@@ -3,6 +3,35 @@ load.project()
 
 # pilot analysis
 read.csv("data/raw/pilot/mydata (2).csv") -> pdata
-pdata |> View(
+
+pdata |> d
+
+map
+
+\(pdata){
+
+  # Demographics / static info
+  pdata |>
+    # Not experimental trials
+    filter(!(trial_info %in% c("Diagnostic trial", "Diagnostic instructions",
+                               "Inducer instructions", "Inducer trial",
+                               "Fixation - short", "Fixation - long"))) |>
+    # Remove empty columns
+    select(where(\(x) any(!is.na(x) & x != ""))) |> select(!response)
+
+
+
+
+  # Data
+  pdata$stimulus[pdata$trial_info=="Diagnostic instructions"]
+
+  pdata |>
+    # Only experimental trials
+    filter(!is.na(inducer_run) | trial_info=="Diagnostic instructions") |>
+    # Remove empty columns
+    select(where(\(x) any(!is.na(x) & x != ""))) |> View()
+
+
+
 
 )
