c8dabe33ed1657186b2fbe4668506d56af8b53e2
steff <98240487+a-asen@users.noreply.github.com>
2023-11-10 07:43:15 +0100

    demo data & experiment data

--- Analysis - R/src/pilot testing.R
+++ Analysis - R/src/pilot testing.R
@@ -4,12 +4,7 @@ load.project()
 # pilot analysis
 read.csv("data/raw/pilot/mydata (2).csv") -> pdata
 
-pdata |> d
-
-map
-
-\(pdata){
-
+demo_data <- \(pdata){
   # Demographics / static info
   pdata |>
     # Not experimental trials
@@ -19,19 +14,17 @@ map
     # Remove empty columns
     select(where(\(x) any(!is.na(x) & x != ""))) |> select(!response)
 
+}
 
-
-
+exp_data <- \(pdata){
   # Data
-  pdata$stimulus[pdata$trial_info=="Diagnostic instructions"]
+  pdata$stimulus[pdata$trial_info=="Diagnostic instructions"] -> diagnostic_instruction
 
   pdata |>
     # Only experimental trials
-    filter(!is.na(inducer_run) | trial_info=="Diagnostic instructions") |>
+    filter(!is.na(inducer_run) | trial_info == "Diagnostic instructions") |>
     # Remove empty columns
-    select(where(\(x) any(!is.na(x) & x != ""))) |> View()
+    select(where(\(x) any(!is.na(x) & x != "")))
 
+}
 
-
-
-)
