e4a6876f76ff2c15e01a8d63e200da89b0113ce8
steff <98240487+a-asen@users.noreply.github.com>
2023-12-04 14:37:27 +0100

    analysis pilot

--- Analysis - R/src/pilot testing.R
+++ Analysis - R/src/pilot testing.R
@@ -2,7 +2,79 @@ library(ProjectTemplate)
 load.project()
 
 # pilot analysis
-read.csv("data/raw/pilot/mydata (2).csv") -> pdata
+read.csv("data/raw/pilot/mydata2.csv") -> data
+data |>
+  mutate(test = case_when(trial_info=="Diagnostic trial" ~ correct_response,
+                          trial_info=="Inducer trial" ~ inducer_correct_response,
+                          T~NA))
+
+#   Get files     =====
+list.files("data/raw/experiment1", pattern = "*.csv", full.names = T) -> fnames
+#list.files("data/raw/experiment2", pattern = "*.csv")
+
+map_df(fnames, \(x){
+  read_csv(x)
+}) -> data
+
+
+#   Exclusion       =====
+# Practice round
+data |>
+  select(id, trial_info, inducer_run, correct_response, inducer_correct_response, rt, congruent) |>
+  filter(inducer_run>0 & !(trial_info=="Inducer instructions"))
+  mutate(correct_response = as.integer(ifelse(correct_response == "true", 1,0)),
+         rt = as.integer(ifelse(rt=="null", NA, rt)),
+#         )-> data
+
+# Overall accuracy
+data |>
+  filter(trial_info=="Diagnostic trial" | trial_info == "Inducer trial") |>
+  group_by(id) |>
+  summarize(acc = sum(correct_response)/length(correct_response)) |>
+  filter(acc<.7) |>
+  pull(id) -> exclude_par
+
+# Trials more than 2.5 SD
+data |>
+  mutate(rt_crit = mean(rt, na.rm=T), rt_crit_sd=sd(rt, na.rm=T), rt_exc=rt_crit+rt_crit_sd*2.5) |>
+  filter(rt>rt_exc)
+
+
+  # filter(!(id %in% exclude_par)) |>
+  select(id, inducer_run, trial_info, congruent, rt, correct_response) |>
+  filter(trial_info=="Diagnostic trial" | trial_info == "Inducer trial") |>
+
+  group_by(id, trial_info) |>
+  summarize(rt = sd(rt, na.rm=T),
+            )
+
+
+
+
+# diagnostic RT & accuracy
+pdata |>
+  select(id, inducer_run, trial_info, congruent, rt, correct_response) |>
+  filter(trial_info=="Diagnostic trial" & inducer_run>0) |>
+  mutate(),
+         correct_response = ifelse(correct_response=="true", 1,0)) |>
+  group_by(id, congruent) |>
+  summarize(rt = mean(rt, na.rm=T), accuracy = sum(correct_response)/length(correct_response))
+
+pdata |>
+  select(id, inducer_run, trial_info, congruent, rt, correct_response) |>
+  filter(trial_info=="Diagnostic trial" & inducer_run>0) |>
+  mutate(rt = as.integer(ifelse(rt=="null",NA,rt)),
+         correct_response = ifelse(correct_response=="true", 1,0)) |>
+  ggplot(aes(x=congruent, y=rt, col=congruent))+
+  geom_point()
+
+
+pdata[pdata$trial_info=="Diagnostic trial",]  |>
+  mutate(rt = as.integer(ifelse(rt=="null", NA, rt)))
+  group_by(id, congruent) |> View()
+  summarize(rt = mean(rt, na.rm=T))
+
+unique(pdata$trial_info)
 
 demo_data <- \(pdata){
   # Demographics / static info
@@ -16,6 +88,7 @@ demo_data <- \(pdata){
 
 }
 
+demo_data(pdata)
 d -> pdata
 
 exp_data <- \(pdata, rm_instr = F ){
