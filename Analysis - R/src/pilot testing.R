8901a5683618c315f80d0d03508670ae574c586e
steff <98240487+a-asen@users.noreply.github.com>
2024-01-10 22:01:00 +0100

    data addition

--- Analysis - R/src/pilot testing.R
+++ Analysis - R/src/pilot testing.R
@@ -12,37 +12,42 @@ data |>
 
 
 #   Get files     =====
+list.files("data/raw/pilot", pattern = "*.csv", full.names = T) -> fnames
 list.files("data/raw/experiment1", pattern = "*.csv", full.names = T) -> fnames
 #list.files("data/raw/experiment2", pattern = "*.csv")
 
-map_df(fnames, \(x){
+fnames2 <- fnames[4:5]
+map_df(fnames2, \(x){
   read_csv(x)
 }) -> data
 
 
 # Pre-transformation
 data |>
-  mutate(rt = as.integer(ifelse(rt=="null", NA, rt))) -> data
+  mutate(rt = as.integer(ifelse( rt=="null", NA, rt ))) -> data
 
 #   Behaviour       =====
 data |>
-  select(id, trial_info, inducer_run, correct_response, rt, congruent) |>
+  select(id, trial_info, inducer_run, correct_response, rt, congruent) |> view()
   filter(trial_info=="Diagnostic trial" | trial_info=="Inducer trial") -> d
+
+# d |> filter(!inducer_run=="practice" ) -> d
 
 ##   Exclusion       =====
 loss <- list()
 loss$data_trials <- nrow(d)
+
 ### Practice round      =====
-d |>
-  filter(inducer_run>0) -> d
+# d |> filter(inducer_run > 0) -> d
+
 
 ### Overall accuracy      ====
 d |>
   filter(trial_info=="Diagnostic trial" | trial_info == "Inducer trial") |>
-  group_by(id) |>
-  summarize(acc = sum(correct_response)/length(correct_response)) |>
-  filter(acc<.7) |>
-  pull(id) -> loss$exclude_par
+  group_by( id ) |>
+  summarize( acc = sum(correct_response)/length(correct_response) ) |>
+  filter( acc < .7 ) |>
+  pull( id ) -> loss$exclude_par
 
 cat(length(loss$exclude_par), " participant(s) have been excluded")
 
@@ -52,7 +57,7 @@ d |> filter(!(id %in% loss$exclude_par)) -> d
 ###  Trials more than 2.5 SD    ======
 d |>
   group_by(id) |>
-  mutate(rt_crit = mean(rt, na.rm=T) + sd(rt, na.rm=T)*2.5) -> d
+  mutate(rt_crit = mean(rt, na.rm = T) + sd(rt, na.rm = T) * 2.5) -> d
 
 cat(sum(d$rt >= d$rt_crit), "lost trial(s) from ", nrow(d),"trials.",
     "Resulting in a loss of", sum(d$rt >= d$rt_crit) / nrow(d) * 100, "percent of the data") # print
@@ -60,39 +65,44 @@ sum(d$rt >= d$rt_crit) / nrow(d) -> loss[["rt_sd_trials"]]
 sum(d$rt >= d$rt_crit) / nrow(d) * 100 -> loss[["rt_sd_pct"]]
 
 d |>
-  filter(rt<rt_crit) -> d
+  filter( rt < rt_crit ) -> d
 
 
 ### Only correct inducers         =====
   # Group_by should still be active?
 d |>
   mutate(valid_trials = case_when(trial_info=="Inducer trial" & correct_response==1 ~ 1,
-                                   trial_info=="Inducer trial" & correct_response==0 ~ 0,
+                                  trial_info=="Inducer trial" & correct_response==0 ~ 0,
                                    T ~ NA)) |>
-  fill(valid_trials, .direction ="up") -> d
+  fill(valid_trials, .direction = "up") -> d
 
 cat("Removed", sum(d$valid_trials==0), "trials of", nrow(d), "trials. Resulting in a loss of",
     sum(d$valid_trials==0) / length(d$valid_trials) * 100, "percent of the data.")
+# save loss
 sum(d$valid_trials==0) -> loss[["inducer_fail_trials"]]
 sum(d$valid_trials==0) / length(d$valid_trials) * 100 -> loss$inducer_fail_pct
 
-d |>
-  filter(valid_trials==1) -> d
+d |> filter(valid_trials == 1) -> d
 
 
 # Diagnostic RT & accuracy ====
 d |>
   filter(trial_info=="Diagnostic trial") |>
   group_by(id, congruent) |>
-  summarize(rt = mean(rt, na.rm=T), pct = sum(correct_response==1) / length(correct_response)) -> d2
+  summarize(rt = mean(rt, na.rm = TRUE),
+            pct = sum(correct_response==1) / length(correct_response)) -> d2
 
 ## RT
-aov(rt ~ congruent, d2)
+aov(rt ~ congruent, d2) |> summary()
 
 ## PCT
-aov(pct ~ congruent, d2)
+aov(pct ~ congruent, d2) |> summary()
 
-
+## Plot
+d2 |>
+  ggplot(aes(x = congruent, y = rt))+
+  geom_point(position = position_nudge(x=-.1), alpha = .5)+
+  stat_summary(fun.data = mean_se)
 
 
 
