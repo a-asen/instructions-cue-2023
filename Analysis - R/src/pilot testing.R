30f1f0bcc2c222a8d93d81348a049a769bafd8c2
steff <98240487+a-asen@users.noreply.github.com>
2024-01-11 17:16:34 +0100

    Update analysis script & added Quarto doc

--- Analysis - R/src/pilot testing.R
+++ Analysis - R/src/pilot testing.R
@@ -1,22 +1,12 @@
 library(ProjectTemplate)
 load.project()
 
-# pilot analysis
-read.csv("data/raw/pilot/mydata2.csv") -> data
-data |>
-  mutate(correct_response = case_when(
-    trial_info=="Diagnostic trial" ~ correct_response,
-    trial_info=="Inducer trial" ~ inducer_correct_response,
-    T~NA),
-    correct_response = ifelse(correct_response=="true", 1, 0)) -> data
-
-
 #   Get files     =====
 list.files("data/raw/pilot", pattern = "*.csv", full.names = T) -> fnames
 list.files("data/raw/experiment1", pattern = "*.csv", full.names = T) -> fnames
 #list.files("data/raw/experiment2", pattern = "*.csv")
 
-fnames2 <- fnames[4:5]
+fnames2 <- fnames[4:7]
 map_df(fnames2, \(x){
   read_csv(x)
 }) -> data
@@ -28,10 +18,10 @@ data |>
 
 #   Behaviour       =====
 data |>
-  select(id, trial_info, inducer_run, correct_response, rt, congruent) |> view()
+  select(id, trial_info, inducer_run, correct_response, rt, congruent) |>
   filter(trial_info=="Diagnostic trial" | trial_info=="Inducer trial") -> d
 
-# d |> filter(!inducer_run=="practice" ) -> d
+ d |> filter(!inducer_run=="practice" ) -> d
 
 ##   Exclusion       =====
 loss <- list()
@@ -45,35 +35,44 @@ loss$data_trials <- nrow(d)
 d |>
   filter(trial_info=="Diagnostic trial" | trial_info == "Inducer trial") |>
   group_by( id ) |>
-  summarize( acc = sum(correct_response)/length(correct_response) ) |>
+  summarize( acc = sum(correct_response, na.rm = TRUE) / length( !is.na(correct_response) ) ) |>
   filter( acc < .7 ) |>
   pull( id ) -> loss$exclude_par
 
-cat(length(loss$exclude_par), " participant(s) have been excluded")
+cat( length( loss$exclude_par ), " participant(s) have been excluded")
 
 d |> filter(!(id %in% loss$exclude_par)) -> d
 
 
-###  Trials more than 2.5 SD    ======
+###  Removing trials more than 2.5 SD (from individual mean) & NA RT    ======
 d |>
   group_by(id) |>
-  mutate(rt_crit = mean(rt, na.rm = T) + sd(rt, na.rm = T) * 2.5) -> d
+  mutate(rt_crit = ifelse( trial_info == "Diagnostic trial",
+                           mean( rt, na.rm = TRUE ) + sd( rt, na.rm = TRUE ) * 2.5,
+                           NA ),
+         retain_trials = ifelse(
+           # Remove deviations more than 2.5 SD
+           rt >= rt_crit & trial_info == "Diagnostic trial" |
+             # AND remove slow responses
+             is.na(rt) & trial_info == "Diagnostic trial",
+                             0, 1 ) ) -> d
+sum(d$rt_remove==1)
 
-cat(sum(d$rt >= d$rt_crit), "lost trial(s) from ", nrow(d),"trials.",
-    "Resulting in a loss of", sum(d$rt >= d$rt_crit) / nrow(d) * 100, "percent of the data") # print
+cat(sum(d$rt >= d$rt_crit, na.rm = T), "lost trial(s) from ", nrow(d), "trials.",
+    "Resulting in a loss of", sum(d$rt >= d$rt_crit, na.rm = T) / nrow(d) * 100, "percent of the data") # print
 sum(d$rt >= d$rt_crit) / nrow(d) -> loss[["rt_sd_trials"]]
 sum(d$rt >= d$rt_crit) / nrow(d) * 100 -> loss[["rt_sd_pct"]]
 
 d |>
-  filter( rt < rt_crit ) -> d
+  filter( retain_trials == 1 ) -> d
 
 
 ### Only correct inducers         =====
   # Group_by should still be active?
 d |>
-  mutate(valid_trials = case_when(trial_info=="Inducer trial" & correct_response==1 ~ 1,
-                                  trial_info=="Inducer trial" & correct_response==0 ~ 0,
-                                   T ~ NA)) |>
+  mutate( valid_trials = case_when( trial_info=="Inducer trial" & correct_response==1 ~ 1,
+                                    trial_info=="Inducer trial" & correct_response==0 ~ 0,
+                                    T ~ NA ) ) |>
   fill(valid_trials, .direction = "up") -> d
 
 cat("Removed", sum(d$valid_trials==0), "trials of", nrow(d), "trials. Resulting in a loss of",
@@ -92,20 +91,46 @@ d |>
   summarize(rt = mean(rt, na.rm = TRUE),
             pct = sum(correct_response==1) / length(correct_response)) -> d2
 
-## RT
-aov(rt ~ congruent, d2) |> summary()
 
-## PCT
-aov(pct ~ congruent, d2) |> summary()
-
-## Plot
+# Statistics          ======
+##  RT          =====
+###   Test          ======
 d2 |>
+  pivot_wider( names_from = congruent, values_from = c(rt, pct) ) |>
+  ungroup() |>
+  summarise(
+    pct_incongruent = mean(rt_FALSE),
+    rt_congruent = mean(rt_TRUE),
+    rt_diff = mean(rt_FALSE - rt_TRUE),
+    t = t.test( rt_FALSE, rt_TRUE, paired = TRUE )[["statistic"]],
+    df = t.test( rt_FALSE, rt_TRUE, paired = TRUE )[["parameter"]],
+    p = t.test( rt_FALSE, rt_TRUE, paired = TRUE )[["p.value"]],
+    )
+
+###   Plot          =====
+library(ggpp)
+d2 |>
+  mutate(congruent = ifelse(str_detect(congruent, "TRUE"), "Congruent", "Incongruent")) |>
   ggplot(aes(x = congruent, y = rt))+
-  geom_point(position = position_nudge(x=-.1), alpha = .5)+
-  stat_summary(fun.data = mean_se)
+  geom_point(position = position_dodgenudge(.05, x = .05), alpha = .35)+
+  geom_line(aes(group = id), position = position_dodgenudge(.05, x=.05), alpha = .4)+
+  stat_summary(fun.data = mean_se, col = "red")+
+  labs(x = "", y = "Response time")
 
 
 
+##    Percet correct        ======
+d2 |>
+  pivot_wider( names_from = congruent, values_from = c( rt, pct ) ) |>
+  ungroup() |>
+  summarise(
+    pct_incongruent = mean( pct_FALSE ),
+    pct_congruent = mean( pct_TRUE ),
+    pct_diff = mean( pct_FALSE - pct_TRUE ),
+    t = t.test( pct_FALSE, pct_TRUE, paired = TRUE )[["statistic"]],
+    df = t.test( pct_FALSE, pct_TRUE, paired = TRUE )[["parameter"]],
+    p = t.test( pct_FALSE, pct_TRUE, paired = TRUE )[["p.value"]],
+  )
 
 
 
